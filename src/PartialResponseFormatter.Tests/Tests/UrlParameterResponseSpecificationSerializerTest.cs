1321cdbd14f30a4146d514efe2d6a8ecc4a6db54
Burlakov Nick <burlakov.nick@gmail.com>
2017-08-30 20:05:34 +0500

    Rewrite test with fluent response specification builder

--- src/PartialResponseFormatter.Tests/Tests/UrlParameterResponseSpecificationSerializerTest.cs
+++ src/PartialResponseFormatter.Tests/Tests/UrlParameterResponseSpecificationSerializerTest.cs
@@ -11,9 +11,9 @@ namespace PartialResponseFormatter.Tests.Tests
     {
         [Test]
         [TestCaseSource(nameof(GenerateTestData))]
-        public void Test_Serialize_And_Deserialize(Field[] fields, string expectedSerialization)
+        public void Test_Serialize_And_Deserialize(ResponseSpecification expectedSpecification,
+            string expectedSerialization)
         {
-            var expectedSpecification = new ResponseSpecification { Fields = fields};
             var serializer = new UrlParameterResponseSpecificationSerializer();
 
             var actualSerialization = serializer.Serialize(expectedSpecification);
@@ -21,128 +21,70 @@ namespace PartialResponseFormatter.Tests.Tests
             actualSerialization.ShouldBeEquivalentTo(expectedSerialization);
 
             var actualSpecification = serializer.Deserialize(expectedSerialization);
-            Console.WriteLine($"Actual specification: {JsonConvert.SerializeObject(actualSpecification, Formatting.Indented)}");
+            Console.WriteLine(
+                $"Actual specification: {JsonConvert.SerializeObject(actualSpecification, Formatting.Indented)}");
             actualSpecification.ShouldBeEquivalentTo(expectedSpecification);
         }
 
         private static IEnumerable<TestCaseData> GenerateTestData()
         {
             yield return new TestCaseData(
-                new Field[0],
+                ResponseSpecification.Empty,
                 string.Empty
             ).SetName("Empty");
-            
+
             yield return new TestCaseData(
-                new []
-                {
-                    new Field { Name = "name" },
-                },
+                ResponseSpecification.Field("name").Create(),
                 "name"
             ).SetName("Single field");
-            
+
             yield return new TestCaseData(
-                new []
-                {
-                    new Field { Name = "name1" },
-                    new Field { Name = "name2" },
-                },
+                ResponseSpecification
+                    .Field("name1")
+                    .Field("name2")
+                    .Create(),
                 "name1;name2"
             ).SetName("Two fields");
-            
+
             yield return new TestCaseData(
-                new []
-                {
-                    new Field { Name = "name1", Fields = new [] { new Field {Name = "a"}, new Field {Name = "b"} }},
-                    new Field { Name = "name2", Fields = new [] { new Field {Name = "d"}, new Field {Name = "a"} }},
-                },
+                ResponseSpecification
+                    .Field("name1", _ => _.Field("a").Field("b"))
+                    .Field("name2", _ => _.Field("d").Field("a"))
+                    .Create(),
                 "name1:(a;b);name2:(d;a)"
             ).SetName("Two fields with nested fields");
-            
+
             yield return new TestCaseData(
-                new []
-                {
-                    new Field
-                    {
-                        Name = "root",
-                        Fields = new []
-                        {
-                            new Field
-                            {
-                                Name = "l",
-                                Fields = new []
-                                {
-                                    new Field
-                                    {
-                                        Name = "ll",
-                                        Fields = new []
-                                        {
-                                            new Field
-                                            {
-                                                Name = "lll"
-                                            },
-                                            new Field
-                                            {
-                                                Name = "llr"
-                                            },
-                                        }
-                                    },
-                                    new Field
-                                    {
-                                        Name = "lr",
-                                        Fields = new []
-                                        {
-                                            new Field
-                                            {
-                                                Name = "lrl"
-                                            },
-                                            new Field
-                                            {
-                                                Name = "lrr"
-                                            },
-                                        }
-                                    }
-                                }
-                            },
-                            new Field
-                            {
-                                Name = "r",
-                                Fields = new []
-                                {
-                                    new Field
-                                    {
-                                        Name = "rl",
-                                        Fields = new []
-                                        {
-                                            new Field
-                                            {
-                                                Name = "rll"
-                                            },
-                                            new Field
-                                            {
-                                                Name = "rlr"
-                                            },
-                                        }
-                                    },
-                                    new Field
-                                    {
-                                        Name = "rr",
-                                        Fields = new []
-                                        {
-                                            new Field
-                                            {
-                                                Name = "rrl"
-                                            },
-                                            new Field
-                                            {
-                                                Name = "rrr"
-                                            },
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                },
+                ResponseSpecification
+                    .Field(
+                        "root", root => root
+                            .Field(
+                                "l", l => l
+                                    .Field(
+                                        "ll", ll => ll
+                                            .Field("lll")
+                                            .Field("llr")
+                                    )
+                                    .Field(
+                                        "lr", lr => lr
+                                            .Field("lrl")
+                                            .Field("lrr")
+                                    )
+                            )
+                            .Field(
+                                "r", r => r
+                                    .Field(
+                                        "rl", ll => ll
+                                            .Field("rll")
+                                            .Field("rlr")
+                                    )
+                                    .Field(
+                                        "rr", lr => lr
+                                            .Field("rrl")
+                                            .Field("rrr")
+                                    )
+                            )
+                    ).Create(),
                 "root:(l:(ll:(lll;llr);lr:(lrl;lrr));r:(rl:(rll;rlr);rr:(rrl;rrr)))"
             ).SetName("Binary tree");
         }
